de7bc3a60e35b113e6dee915650d38b97d076b87
ANGIONE FRANCESCO <s262620@po.polito.it>
2019-11-05 17:27:58 +0100

    organized exercises

---
+++ lab1/ex1/ex1_structural_tmax.vhd
@@ -0,0 +1,43 @@
+library IEEE;
+
+use IEEE.std_logic_1164.all;
+
+entity ex1 is
+	port (
+		A, B: in  STD_LOGIC;
+		S, C: out STD_LOGIC );
+end ex1;
+
+architecture structural of ex1 is
+	signal net1: STD_LOGIC;
+	signal net2: STD_LOGIC;
+	signal net3,net1_1,net1_2,net1_3,net1_4: STD_LOGIC;
+
+--	component Nand_Gate is 
+--		port ( I1, I2: in STD_LOGIC; O: out STD_LOGIC );
+--	end component;
+
+begin
+--	U1: Nand_Gate port map (I1 => A,    I2 => B,    O => net1);
+--	U2: Nand_Gate port map (I1 => A,    I2 => net1, O => net2);
+--	U3: Nand_Gate port map (I1 => net1, I2 => B,    O => net3);
+--	U4: Nand_Gate port map (I1 => net2, I2 => net3, O => S);
+--	U5: Nand_Gate port map (I1 => net1, I2 => net1, O => C);
+
+-- modified version for adding pin faults
+	U1: Nand_Gate port map (I1 => A,    I2 => B,    O => net1);
+	U2: Nand_Gate port map (I1 => A,    I2 => net1_1, O => net2);
+	U3: Nand_Gate port map (I1 => net1_2, I2 => B,    O => net3);
+	U4: Nand_Gate port map (I1 => net2, I2 => net3, O => S);
+	U5: Nand_Gate port map (I1 => net1_3, I2 => net1_4, O => C);
+
+		net1_1<=net1;
+		net1_2<=net1;
+		net1_3<=net1;
+		net1_4<=net1;
+
+
+
+
+
+end structural;
---
+++ lab1/ex1/ex1_synth.v
@@ -0,0 +1,18 @@
+/////////////////////////////////////////////////////////////
+// Created by: Synopsys DC Ultra(TM) in wire load mode
+// Version   : O-2018.06-SP4
+// Date      : Tue Nov  5 10:22:01 2019
+/////////////////////////////////////////////////////////////
+
+
+module ex1 ( A, B, S, C );
+  input A, B;
+  output S, C;
+  wire   N0, n3;
+  assign C = N0;
+
+  AND_GATE U4 ( .I1(A), .I2(B), .O(N0) );
+  NOR_GATE U5 ( .I1(A), .I2(B), .O(n3) );
+  NOR_GATE U6 ( .I1(N0), .I2(n3), .O(S) );
+endmodule
+
