ce582607828b49c765af03e84e62ac7dfcea51b5
ANGIONE FRANCESCO <s262620@po.polito.it>
2019-11-06 13:26:35 +0100

    working on ex52

---
+++ lab1/ex5_2/scripts/synthesis_script.tcl
@@ -0,0 +1,20 @@
+set search_path {..}
+
+uplevel #0 source ../../library/pdt2002.dc_setup.tcl
+
+analyze -library work -format vhdl circ1_beh.vhd
+elaborate ex5_1
+#create_clock -name "clk" -period 200 -waveform { 100 200 } { clk }
+#create_clock -name "rst" -period 200 -waveform { 100 200 } { rst }
+
+#compile -exact_map
+compile_ultra
+
+report_design
+report_area -nosplit
+#report_clock -nosplit
+
+write -hierarchy -format verilog -output ../ex5_1_synth.v
+write -hierarchy -format vhdl -output ../ex5_1_synth.vhd
+
+quit
