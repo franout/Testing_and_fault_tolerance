7d7475a92e96230e66cebf8ed2feb392a35e4504
ANGIONE FRANCESCO <s262620@po.polito.it>
2019-11-14 18:32:26 +0100

    done

--- lab3/ex2_1.sh
+++ lab3/ex2_1.sh
@@ -1,6 +1,20 @@
 #|/bin/sh
 
-./compile_sbst_c.sh ./sbst_c/main.c
+PATH_CORE="./"
+PATH_SBST="./sbst_c/"
+#preparing tb
+"$PATH_CORE"compile_testbench.sh
 
-./run_gate_nogui.sh sbst_c/sbst.hex
- ./run_fsim.sh
+# compiling source file in eVCD extendent value change dumo
+"$PATH_CORE"compile_sbst_c.sh "$PATH_SBST"main.c
+
+# run simulation logic
+# it records the values of PIs and POs
+"$PATH_CORE"run_gate_nogui.sh "$PATH_CORE"sbst_c/sbst.hex
+
+# run fault simulation
+ "$PATH_CORE"run_fsim.sh 
+
+
+# clean up the mess 
+"$PATH_CORE"clean_all.sh
