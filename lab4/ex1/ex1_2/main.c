acbe06013086f7305bd9e55bad04672c1a24324e
ANGIONE FRANCESCO <s262620@po.polito.it>
2019-11-15 18:53:52 +0100

    added sbst for alu

--- lab4/ex1/ex1_2/main.c
+++ lab4/ex1/ex1_2/main.c
@@ -1,21 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*The ALU can be tested by properly executing arithmetic and logic operations with suitable values.
- * The final program can be composed by several implementation of the following template,
- * in which a generic 3-register instruction ALU_OP is used:
- * 2
- * Write VALUEA in REGA
- * Write VALUEB in REGB
- * ALU_OP (dest: REGC, op1: REGA, op2: REGB)
- * Store REGC in memory
- * ALU_OP (dest: REGC, op1: REGB, op2: REGA)
- * Store REGC in memory
- * ALU_OP: addition, subtraction, bitwise AND, bitwise OR, bitwise XOR…
- * Try to use the same patterns used for the register file also for the ALU.*/
+/*
+ *
+Write VALUEA in REGA
+Write VALUEB in REGB
+ALU_OP (dest: REGC, op1: REGA, op2: REGB)
+Store REGC in memory
+ALU_OP (dest: REGC, op1: REGB, op2: REGA)
+Store REGC in memory
+ *
+ * */
 
+/* NOTE:  x stands for integer registers
+ * 	f for floating point registers
+ *
+ * */
+/*register x0 is read only */
 
 int main(int argc, char *argv[])
 {
+	int pattern [6] = {0x00000000, 0x0000FFFF, 0x00FF00FF, 0x0F0F0F0F, 0x33333333, 0x55555555};	
+	int i;
+	volatile int res;
+		
+	/*testing integer ALU */
+
+	for ( i=0;i<6;i++) {
+	// addition 	
+	register int a asm("x1")=pattern[i];
+	register int b asm("x2")=pattern[i+1];
+	register int c asm("x3")=a+b;
+	res=c;
+	c=b+a;
+	res=c;
+	// subtraction
+	c=a-b; 
+	res=c;
+	c=b-a;
+	res=c;
+	
+
+	//not
+	c=~a; 
+	res=c;
+	c=~b;
+	res=c;
+	
+	//and
+	c=a&b; 
+	res=c;
+	c=b&a;
+	res=c;
+	 
+	//or
+	c=a|b; 
+	res=c;
+	c=b|a;
+	res=c;
+	
+	//xor
+	c=a^b; 
+	res=c;
+	c=b^a;
+	res=c;
+	
+	//shift left	
+	c=a<<b; 
+	res=c;
+	c=b<<a;
+	res=c;
+	
+	//shift right 
+	c=a>>b; 
+	res=c;
+	c=b>>a;
+	res=c;
+	
+	}
+
 	return EXIT_SUCCESS;
 }
