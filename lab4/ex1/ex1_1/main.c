f1e2c39d1d2136d3446015f6d6b3d7a3d48b406a
ANGIONE FRANCESCO <s262620@po.polito.it>
2019-11-27 19:28:45 +0100

    40 per coverage

--- lab4/ex1/ex1_1/main.c
+++ lab4/ex1/ex1_1/main.c
@@ -32,613 +32,67 @@ int main(int argc, char *argv[])
 	// hint to the compiler for putting the variable into a register (ATTENTION it will not force it ) 
 	/*testing integer register file with differetnt pattern for each register */
 	for ( i=0;i<N_PATT;i++) {
-		register int x1 asm("x1");
 
+
+		register int x ;
+/*
 		a= pattern[i];	  
-		x1=a;
-		c=x1;	
-
-		a= ~pattern[i];
-		x1=a;
-		c=x1; 
-
-		register int x2 asm("x2");
-
-		a= pattern[i];	  
-		x2=a;
-		c=x2;	
-
-		a= ~pattern[i];
-		x2=a;
-		c=x2; 
-		register int x3 asm("x3");
-
-		a= pattern[i];	  
-		x3=a;
-		c=x3;	
-
-		a= ~pattern[i];
-		x3=a;
-		c=x3; 
-		register int x4 asm("x4");
-
-		a= pattern[i];	  
-		x4=x3=x2=x1=a;
+		asm volatile ("lw x0,%0": : "m" (a));
+		asm volatile ("sw x0,%0":  :"m" (c));
+		a=~pattern[i];	
+		asm volatile ("lw x0,%0":  : "m" (a));
+		asm volatile ("sw x0,%0": : "m" (c));
 	
-		c=x1;
-		c=x2;
-		c=x3;
-		c=x4;	
-
-		a= ~pattern[i];
-		x4=a;
-		c=x4;
-		 
-		register int x5 asm("x5");
 
 		a= pattern[i];	  
-		x5=a;
-		c=x5;	
+		asm volatile ("lb x0,%0": : "m" (a));
+		asm volatile ("sb x0,%0":  :"m" (c));
+		a=~pattern[i];
+		asm volatile ("lb x0,%0":  : "m" (a));
+		asm volatile ("sb x0,%0": : "m" (c));
+*/	
 
-		a= ~pattern[i];
-		x5=a;
-		c=x5; 
-		register int x6 asm("x6");
-
-		a= pattern[i];	  
-		x6=a;
-		c=x6;	
-
-		a= ~pattern[i];
-		x6=a;
-		c=x6; 
-		register int x7 asm("x7");
 
 		a= pattern[i];	  
-		x7=a;
-		c=x7;	
-
-		a= ~pattern[i];
-		x7=a;
-		c=x7; 
-		register int x8 asm("x8");
-
+		asm volatile ("lw %0,%1": "=r" (x) : "m" (a));
+		asm volatile ("sw %0,%1": "=r" (x)  :"m" (c));
+		a=~pattern[i];	
+		asm volatile ("lw %0,%1": "=r" (x) : "m" (a));
+		asm volatile ("sw %0,%1": "=r" (x) : "m" (c));
 		a= pattern[i];	  
-		x8=a;
-		c=x8;	
+		asm volatile ("lb %0,%1": "=r" (x) : "m" (a));
+		asm volatile ("sb %0,%1": "=r" (x)  :"m" (c));
+		a=~pattern[i];
+		asm volatile ("lb %0,%1": "=r" (x) : "m" (a));
+		asm volatile ("sb %0,%1": "=r" (x) : "m" (c));
+	
 
-		a= ~pattern[i];
-		x8=a;
-		c=x8; 
-		register int x9 asm("x9");
+// special registers
+/*				a=pattern[i];
+		asm volatile ("csrrs x11, csr, x0");
+		asm volatile ("csrrw x0,csr,%0"::"i" (0x0));			
+		a=~pattern[i];
+		asm volatile ("csrrw x0,csr,%0":: "i" (0xf));			
 
-		a= pattern[i];	  
-		x9=a;
-		c=x9;	
+*/		/*csrrs rd, csr, x0 Read CSR
+		  csrw csr, rs csrrw x0, csr, rs Write CSR
+		  csrs csr, rs csrrs x0, csr, rs Set bits in CSR
+		  csrc csr, rs csrrc x0, csr, rs Clear bits in CSR
+		  csrwi csr, imm csrrwi x0, csr, imm Write CSR, immediate
+		  csrsi csr, imm csrrsi x0, csr, imm Set bits in CSR, immediate
+		  csrci csr, imm csrrci x0, csr, imm Clear bits in CSR, immediate
+		  */
+		// special load store instructions 
+		/*		a=pattern[i];
+				asm volatile ("la %0,%1" : "=r" (x): "g" (a));
+				a=~pattern[i];
+				asm volatile ("la x%0,%1" : "=r" (x): "g" (a));
 
-		a= ~pattern[i];
-		x9=a;
-		c=x9; 
-		register int x10 asm("x10");
 
-		a= pattern[i];	  
-		x10=a;
-		c=x10;	
-
-		a= ~pattern[i];
-		x10=a;
-		c=x10; 
-		register int x11 asm("x11");
-
-		a= pattern[i];	  
-		x11=a;
-		c=x11;	
-
-		a= ~pattern[i];
-		x11=a;
-		c=x11; 
-		register int x12 asm("x12");
-
-		a= pattern[i];	  
-		x12=a;
-		c=x12;	
-
-		a= ~pattern[i];
-		x12=a;
-		c=x12; 
-		register int x13 asm("x13");
-
-		a= pattern[i];	  
-		x13=a;
-		c=x13;	
-
-		a= ~pattern[i];
-		x13=a;
-		c=x13; 
-		register int x14 asm("x14");
-
-		a= pattern[i];	  
-		x14=a;
-		c=x14;	
-
-		a= ~pattern[i];
-		x14=a;
-		c=x14; 
-		register int x15 asm("x15");
-
-		a= pattern[i];	  
-		x15=a;
-		c=x15;	
-
-		a= ~pattern[i];
-		x15=a;
-		c=x15; 
-		register int x16 asm("x16");
-
-		a= pattern[i];	  
-		x16=a;
-		c=x16;	
-
-		a= ~pattern[i];
-		x16=a;
-		c=x16; 
-		register int x17 asm("x17");
-
-		a= pattern[i];	  
-		x17=a;
-		c=x17;	
-
-		a= ~pattern[i];
-		x17=a;
-		c=x17; 
-		register int x18 asm("x18");
-
-		a= pattern[i];	  
-		x18=a;
-		c=x18;	
-
-		a= ~pattern[i];
-		x18=a;
-		c=x18; 
-		register int x19 asm("x19");
-
-		a= pattern[i];	  
-		x19=a;
-		c=x19;	
-
-		a= ~pattern[i];
-		x19=a;
-		c=x19; 
-		register int x20 asm("x20");
-
-		a= pattern[i];	  
-		x20=a;
-		c=x20;	
-
-		a= ~pattern[i];
-		x20=a;
-		c=x20; 
-		register int x21 asm("x21");
-
-		a= pattern[i];	  
-		x21=a;
-		c=x21;	
-
-		a= ~pattern[i];
-		x21=a;
-		c=x21; 
-		register int x22 asm("x22");
-
-		a= pattern[i];	  
-		x22=a;
-		c=x22;	
-
-		a= ~pattern[i];
-		x22=a;
-		c=x22; 
-		register int x23 asm("x23");
-
-		a= pattern[i];	  
-		x23=a;
-		c=x23;	
-
-		a= ~pattern[i];
-		x23=a;
-		c=x23; 
-		register int x24 asm("x24");
-
-		a= pattern[i];	  
-		x24=a;
-		c=x24;	
-
-		a= ~pattern[i];
-		x24=a;
-		c=x24; 
-		register int x25 asm("x25");
-
-		a= pattern[i];	  
-		x25=a;
-		c=x25;	
-
-		a= ~pattern[i];
-		x25=a;
-		c=x25; 
-		register int x26 asm("x26");
-
-		a= pattern[i];	  
-		x26=a;
-		c=x26;	
-
-		a= ~pattern[i];
-		x26=a;
-		c=x26; 
-		register int x27 asm("x27");
-
-		a= pattern[i];	  
-		x27=a;
-		c=x27;	
-
-		a= ~pattern[i];
-		x27=a;
-		c=x27; 
-		register int x28 asm("x28");
-
-		a= pattern[i];	  
-		x28=a;
-		c=x28;	
-
-		a= ~pattern[i];
-		x28=a;
-		c=x28; 
-		register int x29 asm("x29");
-
-		a= pattern[i];	  
-		x29=a;
-		c=x29;	
-
-		a= ~pattern[i];
-		x29=a;
-		c=x29; 
-		register int x30 asm("x30");
-
-		a= pattern[i];	  
-		x30=a;
-		c=x30;	
-
-		a= ~pattern[i];
-		x30=a;
-		c=x30; 
-		register int x31 asm("x31");
-
-		a= pattern[i];	  
-		x31=a;
-		c=x31;	
-
-		a= ~pattern[i];
-		x31=a;
-		c=x31; 
-		register int x32 asm("x32");
-
-		a= pattern[i];	  
-		x32=a;
-		c=x32;	
-
-		a= ~pattern[i];
-		x32=a;
-		c=x32; 
-		register  int x0 asm("x0");
-
-		a= pattern[i];	  
-		x0=a;
-		c=x0;	
-
-		a= ~pattern[i];
-		x0=a;
-		c=x0; 	
+				a=pattern[i];
+				asm volatile ("lla %0,%1" : "=r" (x): "g" (a));
+				a=~pattern[i];
+				asm volatile ("lla %0,%1" : "=r" (x): "g" (a));  */
 	}
-	/*testing floating point register file */
-	for( i=0;i<N_PATT;i++) {
-		register int x1 asm("f1");
-
-		a= pattern[i];	  
-		x1=a;
-		c=x1;	
-
-		a= ~pattern[i];
-		x1=a;
-		c=x1; 
-
-		register int x2 asm("f2");
-
-		a= pattern[i];	  
-		x2=a;
-		c=x2;	
-
-		a= ~pattern[i];
-		x2=a;
-		c=x2; 
-		register int x3 asm("f3");
-
-		a= pattern[i];	  
-		x3=a;
-		c=x3;	
-
-		a= ~pattern[i];
-		x3=a;
-		c=x3; 
-		register int x4 asm("f4");
-
-		a= pattern[i];	  
-		x4=a;
-		c=x4;	
-
-		a= ~pattern[i];
-		x4=a;
-		c=x4; 
-		register int x5 asm("f5");
-
-		a= pattern[i];	  
-		x5=a;
-		c=x5;	
-
-		a= ~pattern[i];
-		x5=a;
-		c=x5; 
-		register int x6 asm("f6");
-
-		a= pattern[i];	  
-		x6=a;
-		c=x6;	
-
-		a= ~pattern[i];
-		x6=a;
-		c=x6; 
-		register int x7 asm("f7");
-
-		a= pattern[i];	  
-		x7=a;
-		c=x7;	
-
-		a= ~pattern[i];
-		x7=a;
-		c=x7; 
-		register int x8 asm("f8");
-
-		a= pattern[i];	  
-		x8=a;
-		c=x8;	
-
-		a= ~pattern[i];
-		x8=a;
-		c=x8; 
-		register int x9 asm("f9");
-
-		a= pattern[i];	  
-		x9=a;
-		c=x9;	
-
-		a= ~pattern[i];
-		x9=a;
-		c=x9; 
-		register int x10 asm("f10");
-
-		a= pattern[i];	  
-		x10=a;
-		c=x10;	
-
-		a= ~pattern[i];
-		x10=a;
-		c=x10; 
-		register int x11 asm("f11");
-
-		a= pattern[i];	  
-		x11=a;
-		c=x11;	
-
-		a= ~pattern[i];
-		x11=a;
-		c=x11; 
-		register int x12 asm("f12");
-
-		a= pattern[i];	  
-		x12=a;
-		c=x12;	
-
-		a= ~pattern[i];
-		x12=a;
-		c=x12; 
-		register int x13 asm("f13");
-
-		a= pattern[i];	  
-		x13=a;
-		c=x13;	
-
-		a= ~pattern[i];
-		x13=a;
-		c=x13; 
-		register int x14 asm("f14");
-
-		a= pattern[i];	  
-		x14=a;
-		c=x14;	
-
-		a= ~pattern[i];
-		x14=a;
-		c=x14; 
-		register int x15 asm("f15");
-
-		a= pattern[i];	  
-		x15=a;
-		c=x15;	
-
-		a= ~pattern[i];
-		x15=a;
-		c=x15; 
-		register int x16 asm("f16");
-
-		a= pattern[i];	  
-		x16=a;
-		c=x16;	
-
-		a= ~pattern[i];
-		x16=a;
-		c=x16; 
-		register int x17 asm("f17");
-
-		a= pattern[i];	  
-		x17=a;
-		c=x17;	
-
-		a= ~pattern[i];
-		x17=a;
-		c=x17; 
-		register int x18 asm("f18");
-
-		a= pattern[i];	  
-		x18=a;
-		c=x18;	
-
-		a= ~pattern[i];
-		x18=a;
-		c=x18; 
-		register int x19 asm("f19");
-
-		a= pattern[i];	  
-		x19=a;
-		c=x19;	
-
-		a= ~pattern[i];
-		x19=a;
-		c=x19; 
-		register int x20 asm("f20");
-
-		a= pattern[i];	  
-		x20=a;
-		c=x20;	
-
-		a= ~pattern[i];
-		x20=a;
-		c=x20; 
-		register int x21 asm("f21");
-
-		a= pattern[i];	  
-		x21=a;
-		c=x21;	
-
-		a= ~pattern[i];
-		x21=a;
-		c=x21; 
-		register int x22 asm("f22");
-
-		a= pattern[i];	  
-		x22=a;
-		c=x22;	
-
-		a= ~pattern[i];
-		x22=a;
-		c=x22; 
-		register int x23 asm("f23");
-
-		a= pattern[i];	  
-		x23=a;
-		c=x23;	
-
-		a= ~pattern[i];
-		x23=a;
-		c=x23; 
-		register int x24 asm("f24");
-
-		a= pattern[i];	  
-		x24=a;
-		c=x24;	
-
-		a= ~pattern[i];
-		x24=a;
-		c=x24; 
-		register int x25 asm("f25");
-
-		a= pattern[i];	  
-		x25=a;
-		c=x25;	
-
-		a= ~pattern[i];
-		x25=a;
-		c=x25; 
-		register int x26 asm("f26");
-
-		a= pattern[i];	  
-		x26=a;
-		c=x26;	
-
-		a= ~pattern[i];
-		x26=a;
-		c=x26; 
-		register int x27 asm("f27");
-
-		a= pattern[i];	  
-		x27=a;
-		c=x27;	
-
-		a= ~pattern[i];
-		x27=a;
-		c=x27; 
-		register int x28 asm("f28");
-
-		a= pattern[i];	  
-		x28=a;
-		c=x28;	
-
-		a= ~pattern[i];
-		x28=a;
-		c=x28; 
-		register int x29 asm("f29");
-
-		a= pattern[i];	  
-		x29=a;
-		c=x29;	
-
-		a= ~pattern[i];
-		x29=a;
-		c=x29; 
-		register int x30 asm("f30");
-
-		a= pattern[i];	  
-		x30=a;
-		c=x30;	
-
-		a= ~pattern[i];
-		x30=a;
-		c=x30; 
-		register int x31 asm("f31");
-
-		a= pattern[i];	  
-		x31=a;
-		c=x31;	
-
-		a= ~pattern[i];
-		x31=a;
-		c=x31; 
-		register int x32 asm("f32");
-
-		a= pattern[i];	  
-		x32=a;
-		c=x32;	
-
-		a= ~pattern[i];
-		x32=a;
-		c=x32; 
-		register  int x0 asm("f0");
-
-		a= pattern[i];	  
-		x0=a;
-		c=x0;	
-
-		a= ~pattern[i];
-		x0=a;
-		c=x0; 
-
-
-	}
-
 	return EXIT_SUCCESS;
 }
