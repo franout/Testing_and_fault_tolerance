fed6db027e482eb5ab72b9670b87c58298604848
ANGIONE FRANCESCO <s262620@po.polito.it>
2019-12-10 20:51:24 +0100

    done

--- lab4/ex2/ex2_2.sh
+++ lab4/ex2/ex2_2.sh
@@ -8,14 +8,14 @@ PATH_SBST="./"
 ./run_atpg.sh
 ./run_parser.sh
 
-mv ./atpg_patterns.S ~/riscv_testing/sbst_asm/atpg_pattern.S
+mv ./atpg_patterns.S ~/riscv_testing/sbst_c/atpg_patterns.S
+mv ./main.c ~/riscv_testing/sbst_c/main.c
 # compiling source file in eVCD extendent value change dumo
-"$PATH_CORE"compile_sbst_asm.sh "$PATH_SBST"main.S
-
-
+"$PATH_CORE"compile_sbst_c.sh "$PATH_SBST"main.c
+mv ~/riscv_testing/sbst_c/main.c ./main.c
 # run simulation logic
 # it records the values of PIs and POs
-"$PATH_CORE"run_gate_nogui.sh sbst_asm/sbst.hex
+"$PATH_CORE"run_gate_nogui.sh sbst_c/sbst.hex
 
 # run fault simulation
  "$PATH_CORE"run_fsim.sh 
